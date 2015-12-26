267208ed3edc9f13ae5f1770b45708dab5c3c97a
Yefei <Yefei@8b56e702-20d0-4c2c-9f52-078ba9e7a547>
2015-12-26 06:43:00 +0000

    更新部署文件

--- start.sh
+++ start.sh
@@ -1,8 +1,10 @@
 uwsgi \
---chdir /www/webapp/ \
--M --pidfile var/uwsgi.pid \
--L -s var/uwsgi.sock \
--p 4 --uid nobody \
---env "DJANGO_SETTINGS_MODULE=webapp.settings" \
--w "django.core.handlers.wsgi:WSGIHandler()" \
--d var/uwsgi.log
+--plugin python \
+--chdir /data/www/ \
+--module=webapp.wsgi:application \
+--master --pidfile=var/uwsgi.pid \
+--processes=4 --enable-threads \
+--disable-logging \
+--daemonize=var/uwsgi.log \
+--socket=/tmp/uwsgi-www.sock \
+--uid nobody
